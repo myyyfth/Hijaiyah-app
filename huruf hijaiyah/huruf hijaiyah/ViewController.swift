bc5a425f1a02ed06921b2b392a9c96c65eb61592
Ahmad Miftah Syakir <ahmadmiftahsyakir@MacBook-Pro-Ahmad.local>
2021-04-11 11:11:09 +0700

    first commit

---
+++ huruf hijaiyah/huruf hijaiyah/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  huruf hijaiyah
+//
+//  Created by Ahmad Miftah Syakir on 20/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
